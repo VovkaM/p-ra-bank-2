ba9d70f7d8815ae74663680fb05ba8ee397ea86a
Иван Николаев <ivanix8@gmail.com>
2023-12-06 17:28:24 +0000

    Closes bk-2

--- antifraud/src/main/java/com/bank/antifraud/AntiFraudApplication.java
+++ antifraud/src/main/java/com/bank/antifraud/AntiFraudApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
 @EnableEurekaClient
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.bank.antifraud", "com.bank.common"})
 public class AntiFraudApplication {
     public static void main(String[] args) {
         SpringApplication.run(AntiFraudApplication.class, args);
